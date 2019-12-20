59f3a6fe35de5a223192126809d19a085876c160
xicoll6 <xicoll6@163.com>
2019-12-20 16:50:55 +0800

    leetcode9 回文数

--- leetcode/src/Solution9.java
+++ leetcode/src/Solution9.java
@@ -1,23 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * LeetCode9：回文数
+ */
 public class Solution9 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(isPalindrome(x));
     }
-    public boolean isPalindrome(int x) {
+
+    public static boolean isPalindrome(int x) {
         boolean isPalindromeX = true;
         String leftX = String.valueOf(x);
         String rightX = String.valueOf(strReverse(String.valueOf(x)));
         if(!leftX.equals(rightX)){
             isPalindromeX = false;
         }
-
         return isPalindromeX;
     }
 
-    //2. StringBuffer的reverse方法
+    // 字符串倒序排列方法
+    // 2. StringBuffer的reverse方法
     public static StringBuffer strReverse(String s){
         StringBuffer sb=new StringBuffer();
         sb.append(s);
         return sb.reverse();
     }
-
 }
